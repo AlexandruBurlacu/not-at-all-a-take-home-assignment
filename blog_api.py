9344093248783a55bdedf8fe7c3b48d782a94e35
Alex Burlacu <alexburlacu1996@gmail.com>
2022-02-02 04:04:30 +0200

    adjust models and split requirements

--- blog_api.py
+++ blog_api.py
@@ -5,16 +5,20 @@ import uuid
 
 app = FastAPI()
 
-class Blogpost(BaseModel):
-    title: str
-    paragraphs: list[str]
+class Blogpost:
+    class Request(BaseModel):
+        title: str
+        paragraphs: list[str]
+    
+    class Response(BaseModel):
+        blogpost_slug: str
 
 
 def prepare_slug_prefix(title: str):
     return title.lower().rstrip(" ").lstrip(" ").replace(" ", "-")
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def post_blogpost(blogpost: Blogpost) -> dict[str, str]:
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=Blogpost.Response)
+def post_blogpost(blogpost: Blogpost.Request) -> Blogpost.Response:
     slug = prepare_slug_prefix(blogpost.title) + f"-{uuid.uuid4()}"
-    return {"blogpost_slug": slug}
+    return Blogpost.Response(blogpost_slug=slug)
