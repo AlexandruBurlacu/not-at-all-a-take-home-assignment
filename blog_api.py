612f5b6d7999e85614df3fa597741c1ded3837cd
Alex Burlacu <alexburlacu1996@gmail.com>
2022-02-02 03:12:43 +0200

    cover an edgecase

--- blog_api.py
+++ blog_api.py
@@ -9,7 +9,12 @@ class Blogpost(BaseModel):
     title: str
     paragraphs: list[str]
 
+
+def prepare_slug_prefix(title: str):
+    return title.lower().rstrip(" ").lstrip(" ").replace(" ", "-")
+
+
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def post_blogpost(blogpost: Blogpost) -> dict[str, str]:
-    slug = blogpost.title.lower().replace(" ", "-") + f"-{uuid.uuid4()}"
+    slug = prepare_slug_prefix(blogpost.title) + f"-{uuid.uuid4()}"
     return {"blogpost_slug": slug}
--- test_blog_api_e2e.py
+++ test_blog_api_e2e.py
@@ -20,3 +20,24 @@ def test_blog_posting_ok():
 
     assert resp.status_code == 201
     assert resp.json().get("blogpost_slug").startswith("dummy-title-")
+
+def test_blog_posting_trailing_whitespace():
+    resp = client.post("/posts", headers={
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }, json={
+        "title": "  Dummy Title   ",
+        "paragraphs": [
+            "Paragraph 1",
+            "Paragraph two",
+            "Final paragraph"
+        ]
+    })
+
+
+    assert resp.status_code == 201
+    assert not resp.json().get("blogpost_slug").startswith("dummy-title--")
+    assert not resp.json().get("blogpost_slug").startswith("--dummy-title--")
+    assert not resp.json().get("blogpost_slug").startswith("  dummy-title--")
+    assert not resp.json().get("blogpost_slug").startswith("dummy-title- ")
+    assert resp.json().get("blogpost_slug").startswith("dummy-title-")
